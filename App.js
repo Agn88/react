b41721d0000eddd001954373a6a0b28fb6a359b7
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-02-24 17:12:20 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './source/components/Header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+       
+        <Header/>
+
+      </View>
+    );
+  }
+}
