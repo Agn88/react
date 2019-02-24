e7c3892e3d169d8dee7ea9c3b18b7986f2085432
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-02-24 17:32:32 -0300

    Renderizando uma lista

--- App.js
+++ App.js
@@ -4,12 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './source/components/Header';
 
 export default class App extends React.Component {
+  renderList(){
+    const names = [
+      'Eddie Van Halen',
+      'Jimi Hendrix',
+      'Chimbinha',
+      'Steve Vai'
+    ];
+
+    const textElements =  names.map(name => {
+      return <Text>{ name }</Text>
+    })
+
+    return textElements;
+  }
+
+
   render() {
     return (
       <View>
        
-        <Header/>
-
+        <Header title="Pessoas!"/>
+        { this.renderList() }
       </View>
     );
   }
