0d2247b3aaf315cfa67f043fe220c6fa9cdf7341
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-02-26 16:35:26 -0300

    Refactoring: Preparando para próximas páginas

--- App.js
+++ App.js
@@ -1,42 +1,4 @@
-import React from 'react';
-import {Text, View } from 'react-native';
-
-import Header from './source/components/Header';
-import PeopleList from './source/components/PeopleList';
-
-import axios from 'axios';
-
-export default class App extends React.Component {
-
-  constructor(props){
-    super(props);
-
-    this.state = {
-      peoples: []
-    };
-  }
-
-  componentDidMount(){
-    /* Promises */
-    axios
-      .get('https://randomuser.me/api/?nat=br&results=5')
-      .then(response => {
-        const { results } = response.data;
-        this.setState({
-          peoples: results
-        });
-    })
-
-  }
+import PeoplePage from './source/pages/PeoplePage';
 
 
-
-  render() {
-    return (
-      <View>
-        <Header title="Pessoas!"/>
-        <PeopleList peoples={this.state.peoples}/>
-      </View>
-    );
-  }
-}
+export default PeoplePage;
