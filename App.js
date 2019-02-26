ab49086a87661b068fb38642e10cb0b690c2d22a
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-02-26 15:20:55 -0300

    PeopleListItem (Cada coisa em seu lugar)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 import Header from './source/components/Header';
 import PeopleList from './source/components/PeopleList';
