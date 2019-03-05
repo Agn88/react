0acbe21f62df536e337fad8eea1cff9dee9b3f13
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-03-04 21:55:19 -0300

    Tratamento de erros

--- source/pages/PeoplePage.js
+++ source/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import {Text, View, ActivityIndicator, StyleSheet} from 'react-native';
 import PeopleList from '../components/PeopleList';
 
 import axios from 'axios';
@@ -10,20 +10,53 @@ export default class PeoplePage extends React.Component {
     super(props);
 
     this.state = {
-      peoples: []
+      peoples: [],
+      loading: false, 
+      error: false,
     };
   }
 
   componentDidMount(){
     /* Promises */
-    axios
-      .get('https://randomuser.me/api/?nat=br&results=150')
-      .then(response => {
-        const { results } = response.data;
-        this.setState({
-          peoples: results
-        });
-    })
+    this.setState({ loading: true});
+    setTimeout(() => {
+
+      axios
+        .get('https://randomuser.me/api/?nat=br&results=150')
+        .then(response => {
+          const { results } = response.data;
+          this.setState({
+            peoples: results,
+            loading: false,
+          });
+      }).catch(error => {
+        this.setState({ 
+          loading: false,
+          error: true 
+        })
+      });
+    }, 1500)
+
+  }
+
+
+  renderPage() {
+    if(this.state.loading)
+      return <ActivityIndicator size="large" color="#6ca2f7" />
+    
+    if(this.state.error)
+      return <Text style={styles.error}>Ops... Algo deu errado =( </Text>; 
+
+
+    return (
+              <PeopleList 
+                peoples={this.state.peoples}
+                onPressItem={pageParams => {
+                this.props.navigation.navigate('PeopleDetail', pageParams);
+              }} />
+
+    );
+
 
   }
 
@@ -33,13 +66,21 @@ export default class PeoplePage extends React.Component {
     //this.props.navigation.navigate(' Chave da página, State')
     //this.props.navigation.navigate('PeopleDetail');
     return (
-        <View>
-            <PeopleList 
-                peoples={this.state.peoples}
-                onPressItem={pageParams => {
-                    this.props.navigation.navigate('PeopleDetail', pageParams);
-                }} />
+        <View style={styles.container}>
+            {  this.renderPage() }
         </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  error: {
+    color: 'red',
+    alignSelf: 'center',
+    fontSize: 18
+  }
+})
